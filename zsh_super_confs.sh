aee4224559829db844ddf1a0a13605001e390e33
Topazstix <r.drone@protonmail.ch>
2023-04-17 14:09:16 -0600

    Minor Changes: * Adjust wget to use new files * Change comments * Add more print statements for user

--- zsh_super_confs.sh
+++ zsh_super_confs.sh
@@ -44,7 +44,7 @@ echo -n "\n\n"
 ## Install oh-my-zsh in custom directory
 echo "Downloading oh-my-zsh"
 echo "Please visit their github and read their docs for extra configs!"
-echo "https://github.com/ohmyzsh/ohmyzsh.git /usr/share/oh-my-zsh"
+echo "https://github.com/ohmyzsh/ohmyzsh.git"
 git clone https://github.com/ohmyzsh/ohmyzsh.git /usr/share/oh-my-zsh
 
 echo -n "\n\n"
@@ -59,28 +59,42 @@ git clone https://github.com/romkatv/powerlevel10k.git /usr/share/oh-my-zsh/cust
 echo -n "\n\n"
 
 ## Download custom configurations for zsh
-## Check if current folder is a git repo, if so, use conf files from there
+## Check if current folder contains our github's conf folder, 
+## if so, use conf files from there
 ## Otherwise download confs with wget
 if [ -d ./confs ]; then
-   echo "Current directory is a git repo"
-   echo "Changing default user in zshrc confs"
-   sed -i "s/DEFAULT_USER=\"\"/DEFAULT_USER=${username}/g" ./confs/user.zshrc
-   
-   echo "Using conf files from current directory"
+   echo "Using local conf files"
    cp ./confs/p10k.root.zsh /root/.p10k.zsh
    cp ./confs/p10k.user.zsh /home/$username/.p10k.zsh
    cp ./confs/user.zshrc /home/$username/.zshrc
    cp ./confs/user.zshrc /root/.zshrc
    cp ./.p10k.zsh /usr/share/oh-my-zsh/custom/
+   
 else
-   echo "Current directory is not a git repo"
    echo "Downloading conf files from github"
 
-   ## Will update with raw github links
-   # mkdir confs
-   # wget https://raw.githubusercontent.com/0x1CA3/auto-configurator/main/.zshrc -O /usr/share/oh-my-zsh/custom/.zshrc
-   # wget https://raw.githubusercontent.com/0x1CA3/auto-configurator/main/.p10k.zsh -O /usr/share/oh-my-zsh/custom/.p10k.zsh
+   wget https://raw.githubusercontent.com/Topazstix/auto-configurator/main/confs/p10k.root.zsh -O /root/.p10k.zsh
+   wget https://raw.githubusercontent.com/Topazstix/auto-configurator/main/confs/p10k.user.zsh -O /home/$username/.p10k.zsh
+   wget https://raw.githubusercontent.com/Topazstix/auto-configurator/main/confs/user.zshrc -O /home/$username/.zshrc
+   sed -i "s/DEFAULT_USER=\"\"/DEFAULT_USER=${username}/g" /home/$username/.zshrc
+   cp /home/$username/.zshrc /root/.zshrc
+
+
 fi
+
+echo -n "\n\n"
+
+## Change permissions on new files
+echo "Changing perms on new files"
+chown -R $username:$username /home/$username/.{zshrc,p10k.zsh}
+chown -R root:root /root/.{zshrc,p10k.zsh}
+
+## Replace default user with $username in new files
+echo "Changing default user in zshrc confs"
+sed -i "s/DEFAULT_USER=\"\"/DEFAULT_USER=${username}/g" /home/$username/.zshrc
+sed -i "s/DEFAULT_USER=\"\"/DEFAULT_USER=${username}/g" /root/.zshrc
+
+
 
 ## Change default shell to zsh for root and user
 chsh -s $(which zsh) $username
@@ -91,3 +105,4 @@ echo -n "\n\n"
 ## Advise user to log out and back in to their current shell for changes to take effect
 echo "Successfully configured zsh! Congrats!"
 echo "Log out and back in to your current shell for changes to take effect"
+echo "Alternatively, you make run `zsh` in your current shell to test it out"
